eeeeaa844ca3721f2538f00a62fd790f16e6216d
Abdelfattah Ragab <fullstackragab@gmail.com>
2021-11-27 02:10:34 +0200

    'init'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/market/bmw-3', pathMatch: 'full' },
+  {
+    path: 'market',
+    loadChildren: () =>
+      import('./market/market.module').then((m) => m.MarketModule),
+  },
+  { path: '**', redirectTo: '/market' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
